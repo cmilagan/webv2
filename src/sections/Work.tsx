2783d9fa9656e1aef0798a49452632c2149d0143
Christian Ilagan <cmilagan7@gmail.com>
2023-01-20 01:15:08 +1100

    Add work section

--- src/sections/Work.tsx
+++ src/sections/Work.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import { BLACK, DARKGREY, WHITE } from "../utils/constants";
 import Section from "../components/section";
 import StyledTitle from "../components/title";
-import { Grid, Container, Typography } from "@mui/material";
+import { Grid, Container, Typography, Stack } from "@mui/material";
+import toshiba from "../assets/images/toshiba.png";
+import trimble from "../assets/images/trimble.png";
+import tyro from "../assets/images/tyro.png";
+import unsw from "../assets/images/unsw.png";
+import styled from "styled-components";
+
+
+const StyledImage = styled.img`
+  float: left;
+  width:  100px;
+  height: 100px;
+  object-fit: scale-down;
+`
 
 const Work = () => {
   return (
@@ -16,16 +29,19 @@ const Work = () => {
             }}
             
           >
-            <Grid
-              container
-              spacing={1}
+            <Stack
+              spacing={8}
             >
-              <Grid item xs={12} textAlign="center">
-                <Typography variant="h5" color={WHITE}>
-                  Experience
-                </Typography>
-              </Grid>              
-            </Grid>
+              <Typography variant="h5" color={WHITE} textAlign="center">
+                I've worked with
+              </Typography>
+              <Stack direction="row" rowGap={3} spacing={6} flexWrap="wrap" justifyContent="space-around">
+                <StyledImage src={unsw} height="80px" /> 
+                <StyledImage src={toshiba} height="80px" /> 
+                <StyledImage src={trimble} height="80px" /> 
+                <StyledImage src={tyro} height="80px" /> 
+              </Stack>
+            </Stack>
         </Container>
       </Section>
     </div>
