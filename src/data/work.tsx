ae422f7486064ce0c0c37a051e6b91dc8744655d
Christian Ilagan <cmilagan7@gmail.com>
2023-02-12 14:17:44 +1100

    Complete work contributions paragraph

--- src/data/work.tsx
+++ src/data/work.tsx
@@ -41,7 +41,7 @@ export const TRIMBLE: Work = {
   company: "Trimble Viewpoint",
   role: "Software Engineering Intern",
   about: "Trimble Viewpoint construction software solutions, part of Trimble Connected Construction, allow contractors to better manage their projects, processes and people, using the data gathered to lower risk and improve margins. Trimble Viewpoint innovations are transforming the construction industry by connecting operations across financial and HR systems, project management tools and mobile field solutions.",
-  description: "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.",
+  description: "As a Software Intern I was heavily placed into project work. Completing POC's and presenting demo's on how transitioning and implementing this project to Trimble Viewpoints project will be beneficial in terms of future scalability and code management. Throughout this internship, I explored and experimented with a variety of different technologies, including that of node, react, express, AWS, Tomcat and springboot. Consuming and defining api endpoints to be displayed on a frontend application.",
   technologies: [
     {
       tech: 'JavaScript',
@@ -82,7 +82,7 @@ export const TOSHIBA: Work = {
   company: "Toshiba",
   role: "Software Engineering Intern",
   about: "Toshiba is a world leader and innovator in pioneering high technology, a diversified manufacturer and marketer of advanced electronic and electrical products spanning from information & communications systems, digital consumer products, electronic devices and components, power systems, to industrial and social infrastructure systems and home appliances",
-  description: "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.",
+  description: "As an Intern I worked heavily on improving the testing suite of new features for our printer drivers and UI. Working within a .NET techstack I was responsible for delivering high quality code that ensured all edge cases and situations were covered in regards to the flow of Toshiba user interface. I also worked on implementing new feautures in Javascript, adding functionality to the application via plugins.",
   technologies: [
     {
       tech: 'JavaScript',
@@ -103,7 +103,7 @@ export const UNSW: Work = {
   company: "UNSW Stimpact Vertically Integrated Project",
   role: "Software Engineer",
   about: "StimPact is a student led project under UNSW's Vertically Integreated Projects program, Where students and lecturers create and develop solutions to solve real world problems. Stimpact aims to address the issue of neuralstimulation, making it more accessible and affordable to researchers whilst still allowing the same functionality (+ more) you would expect from a typical neural stimulator.",
-  description: "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph.",
+  description: "As a team member of Stimpact I was primarily responsible for the software / firmware side of the project. Creating firmware code using PSOC creator in C and maintaining legacy UI based on kivy (python). Throughout this project the software team had to identify the best and most optimal approach/technologies that would suit the needs of the project and be aligned with the skillset of our team members.",
   technologies: [
     {
       tech: 'JavaScript',
